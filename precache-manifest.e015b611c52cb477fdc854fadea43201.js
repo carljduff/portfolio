96f55ac09052213fc1dc0d90d32d8d5b34da3eee
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-08-04 22:46:51 +0000

    Updates

---
+++ precache-manifest.e015b611c52cb477fdc854fadea43201.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d24437202b1c7132853a",
+    "url": "/portfolio/static/css/main.898f5f27.chunk.css"
+  },
+  {
+    "revision": "d24437202b1c7132853a",
+    "url": "/portfolio/static/js/main.d2443720.chunk.js"
+  },
+  {
+    "revision": "1fdff466cc8bc8f1ea79",
+    "url": "/portfolio/static/js/1.1fdff466.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "abc21b31a62b63fc4c1fc410f5ea58e9",
+    "url": "/portfolio/static/media/jordan.abc21b31.png"
+  },
+  {
+    "revision": "aed7deb3f24f8c89a16898f8ec983f7c",
+    "url": "/portfolio/static/media/mindreader.aed7deb3.png"
+  },
+  {
+    "revision": "9f302f288ac2e824991a62f2203bfa2e",
+    "url": "/portfolio/static/media/weather.9f302f28.png"
+  },
+  {
+    "revision": "0cb151c4b45f278abbee1e8ed6f04956",
+    "url": "/portfolio/static/media/tic.0cb151c4.png"
+  },
+  {
+    "revision": "1cb9031dab896f379bb5429ddb3380f2",
+    "url": "/portfolio/static/media/todo.1cb9031d.png"
+  },
+  {
+    "revision": "283ef3d2a9ff6728da621fb6019ed61d",
+    "url": "/portfolio/static/media/opendish.283ef3d2.png"
+  },
+  {
+    "revision": "f23ca89aacae4db23feb2faa6c551721",
+    "url": "/portfolio/static/media/menu.f23ca89a.png"
+  },
+  {
+    "revision": "b73cc0b5b247ff8e80292b52c428d2a9",
+    "url": "/portfolio/index.html"
+  }
+];
