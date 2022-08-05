8a60fe871b3e601201ec5d2a07b6e599645ffed0
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-08-05 18:58:08 +0000

    Updates

---
+++ precache-manifest.ad8bb7fb282d9341d22a6865e160f0cb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "487849ea5c31780df876",
+    "url": "/portfolio/static/css/main.b1def2fc.chunk.css"
+  },
+  {
+    "revision": "487849ea5c31780df876",
+    "url": "/portfolio/static/js/main.487849ea.chunk.js"
+  },
+  {
+    "revision": "ff66d1c01f15bbc8bcfc",
+    "url": "/portfolio/static/js/1.ff66d1c0.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "abc21b31a62b63fc4c1fc410f5ea58e9",
+    "url": "/portfolio/static/media/jordan.abc21b31.png"
+  },
+  {
+    "revision": "9f302f288ac2e824991a62f2203bfa2e",
+    "url": "/portfolio/static/media/weather.9f302f28.png"
+  },
+  {
+    "revision": "1cb9031dab896f379bb5429ddb3380f2",
+    "url": "/portfolio/static/media/todo.1cb9031d.png"
+  },
+  {
+    "revision": "283ef3d2a9ff6728da621fb6019ed61d",
+    "url": "/portfolio/static/media/opendish.283ef3d2.png"
+  },
+  {
+    "revision": "f23ca89aacae4db23feb2faa6c551721",
+    "url": "/portfolio/static/media/menu.f23ca89a.png"
+  },
+  {
+    "revision": "aed7deb3f24f8c89a16898f8ec983f7c",
+    "url": "/portfolio/static/media/mindreader.aed7deb3.png"
+  },
+  {
+    "revision": "0cb151c4b45f278abbee1e8ed6f04956",
+    "url": "/portfolio/static/media/tic.0cb151c4.png"
+  },
+  {
+    "revision": "9439a33147284e484f309d5b8da388a2",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f49957e48a88c49dd26415999a15fabf.js"
+  "/portfolio/precache-manifest.ad8bb7fb282d9341d22a6865e160f0cb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
